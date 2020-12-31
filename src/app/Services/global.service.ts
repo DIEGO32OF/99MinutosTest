a8f3f5479ba8b0db37da9a591950397e2ccf2b92
diego <32820519+DIEGO32OF@users.noreply.github.com>
2020-12-31 10:52:39 -0600

    Update global.service.ts

--- src/app/Services/global.service.ts
+++ src/app/Services/global.service.ts
@@ -1,7 +1,7 @@
 export let GLOBAL={
 
 	url:'https://api.github.com/repos/DIEGO32OF/99MinutosTest/',
-	token: 'dc82217c7e599d43c50a3ba4a51628106c031a02'
+	token: 'f804046078318a3c7f76dcadcc6fa858f26524d1'
 	
 	
 }
